acdddce4f6165c64e5d54ae9cde67017f72126e9
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-05-30 13:02:53 -0700

    wrote test for Manager class/ similar to engineer and intern so it was kind of copy paste alter

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,21 @@
 /*This is the test file for Manager using jest to test the Manager class */
+const Manager = require("../lib/Manager");
+
+//Testing constructor if it can set a value for Managers officeRoom
+test("Set Manager officeRoom account using constructor", () =>{
+    const oNum = 300;
+    const employee = new Manager("Aniruddha", "abc314@gmail.com", 101, oNum);
+    expect(employee.office).toBe(oNum);
+});
+//Testing getter function to see if it can get officeRoom value
+test("Get Manager officeRoom  value", () =>{
+    const oNum = 303;
+    const employee = new Manager("Mario", "LetsAGo@nintendo.com", 1, oNum);
+    expect(employee.getOnumber()).toBe(oNum);
+});
+//Testing if getEType() returns the correct value
+test("getEType() returns type - \"Manager\"", () =>{
+    const tType = "Manager";
+    const employee = new Manager("Aniruddha","abc314@gmail.com", 1234321, 1);
+    expect(employee.getEType()).toBe(tType);
+});
